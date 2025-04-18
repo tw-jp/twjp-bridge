8c089636bf36384462a27be0e2ae88fb9ab57863
佐料 <78782311+westleft@users.noreply.github.com>
2025-04-18 21:06:52 +0800

    fix: storybook bug

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,19 @@
-import type { StorybookConfig } from '@storybook-vue/nuxt';
+import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  stories: [
+    '../**/*.mdx',
+    '../**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [{
-    "name": "@storybook/addon-essentials",
-    "options": {
-      "docs": false
-    }
-  }, "@chromatic-com/storybook", "@storybook/experimental-addon-test"],
-  "framework": {
-    "name": "@storybook-vue/nuxt",
-    "options": {}
-  }
-};
-export default config;
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      docs: false,
+    },
+  }, '@storybook/addon-onboarding', '@chromatic-com/storybook', '@storybook/experimental-addon-test'],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+}
+export default config
--- components/modal/login.vue
+++ components/modal/login.vue
@@ -12,22 +12,7 @@ onMounted(() => {
 <template>
   <dialog ref="dialogNode" class="dialog" @click.self="dialogNode!.close()">
     <div class="dialog__inner">
-      <!-- <NuxtImg class="dialog__logo" src="/images/Vertical_P_White.png" alt="logo" /> -->
-      <button class="dialog__btn" @click="handleGoogleLogin">
-        <img class="dialog__icon" src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="">
-        使用 Google 登入
-      </button>
-      <button class="dialog__btn btn-disabled" disabled>
-        <img class="dialog__icon" src="https://cdn-icons-png.flaticon.com/512/5968/5968764.png" alt="">
-        使用 Facebook 登入
-      </button>
-      <p class="dialog__text">
-        繼續登入，代表您同意
-        <NuxtLink to="/">
-          服務條款
-        </NuxtLink>
-        。
-      </p>
+      hello world
     </div>
   </dialog>
 </template>
@@ -54,65 +39,6 @@ onMounted(() => {
     flex-direction: column;
     padding: 2vw;
     width: 100%;
-    background-image: url('https://i.imgur.com/jV0b035.jpeg');
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-}
-
-.dialog {
-  &__logo {
-    width: 90%;
-    margin-bottom: 1vw;
-  }
-
-  &__btn {
-    @include flex();
-    font-size: 1vw;
-    width: 100%;
-    color: #333;
-    background-color: #f6f6f6;
-    border: none;
-    padding: 0.4vw 2vw;
-    border-radius: 40px;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
-    margin-bottom: 0.8vw;
-    cursor: pointer;
-    transition: 0.2s;
-    &:hover {
-      background-color: #f0f0f0;
-    }
-    &:focus {
-      outline: none;
-    }
-    &:last-of-type {
-      margin-bottom: 1.6vw;
-    }
-  }
-
-  &__icon {
-    width: 1.4vw;
-    margin-right: 8px;
-  }
-
-  &__text {
-    font-size: 0.8vw;
-    color: #fff;
-    a {
-      color: #fdfcfb;
-    }
-  }
-}
-
-.btn-disabled {
-  cursor: not-allowed;
-  background-color: #a9a9a9;
-  color: #666;
-  &:hover {
-    background-color: #a9a9a9;
-  }
-  .dialog__icon {
-    opacity: 0.6;
   }
 }
 
